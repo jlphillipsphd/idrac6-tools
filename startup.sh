bf219ae3d7b0e9f2c49e9bc574af1e1b7f8fd8d3
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2022-08-29 10:31:50 -0500

    Update resolution on virtual screen

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export DISPLAY=:99
-xvfb-run --auth-file=/Xauthority openbox-session &
+xvfb-run --auth-file=/Xauthority --server-args="-screen 0 1024x768x24" openbox-session &
 x11vnc -forever -display :99 -auth /Xauthority
 
